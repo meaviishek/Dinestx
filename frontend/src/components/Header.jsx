9f43247488bb38e66d0a9ab8a55e306679d48f1a
v-ishnu <vishnuprakadh572@gmail.com>
2024-11-01 01:50:55 +0530

    Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -97,7 +97,7 @@ function Header() {
             </nav>
             <div className="flex items-center justify-end gap-3">
                
-                <a className=" animate-spin inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 "
+                <a className="animate-spin inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 "
                     href="/login">Join Us</a>
             </div>
         </div>
